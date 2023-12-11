3b7447868287d226c0f5402d59562be55b4b3ec7
lucas mialichi <lmcmialichi@gmail.com>
2023-12-11 15:39:40 -0300

    fix

--- tests/run.php
+++ tests/run.php
@@ -10,18 +10,6 @@ $qb = new QueryBuilder(
         "127.0.0.1",
         "root",
         123,
-        "tim_visits"
+        ""
     )
 );
-
-$success = $qb->select([
-    "accomplished_km"
-    ])
-    ->from("visit")
-    ->where("id", ">", ":ID")
-    ->where(fn($exp) => $exp->between("accomplished_km", 1, 2000))
-    ->addParam(":ID",  1)
-    ->toSql();
-
-    dd($success);
-// dd($success->fetch());
